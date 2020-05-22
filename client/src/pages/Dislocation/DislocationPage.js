57bf3fa51ec5c3fbbadc7a6e1ecfa64d4af079f6
Dinmukhamed <omarbaev95@mail.ru>
2020-05-22 16:13:41 +0600

    отчеты csv и еxcel

--- client/src/pages/Dislocation/DislocationPage.js
+++ client/src/pages/Dislocation/DislocationPage.js
@@ -7,6 +7,7 @@ import { WagonTrackingPostPage } from './WagonTrakingPostPage';
 import useSortableData from '../../components/Function/userSortableData';
 import mySearchFunction from '../../components/Function/mySearchFunction';
 import {CSVLink} from "react-csv";
+import ReactExport from "react-export-excel";
 
 export const DislocationPage = () => {
     const [wagons, setWagons] = useState([]);
@@ -14,6 +15,10 @@ export const DislocationPage = () => {
     const [wagonsPerPage, setWagonsPerPage] = useState(50);
     const {loading, request} = useHttp();
     const {token,userId, darkMode} = useContext(AuthContext);
+
+    const ExcelFile = ReactExport.ExcelFile;
+    const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+    const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
     const fetchDislocation = useCallback(async () =>{
         try{
@@ -104,13 +109,37 @@ export const DislocationPage = () => {
                             </div>
                             <div className="div-btn-excel">
                                     <button className={darkMode ? "btn-floating waves-effect waves-light btn-add-user-table-dark btn modal-trigger" : "btn-floating waves-effect waves-light btn-add-user btn modal-trigger"}>
-                                        <i className="material-icons"><CSVLink data={wagons} target="_blank" separator={";"}><img alt="csv" src="https://img.icons8.com/officel/24/000000/export-csv.png"/></CSVLink></i>            
+                                        <i className="material-icons">
+                                            <CSVLink data={wagons} target="_blank" separator={";"}>
+                                                <img alt="csv" src="https://img.icons8.com/officel/24/000000/export-csv.png"/>
+                                            </CSVLink>
+                                        </i>            
                                    </button>
                             </div>
                             <div className="div-btn-excel">
-                                    <button className={darkMode ? "btn-floating waves-effect waves-light btn-add-user-table-dark btn modal-trigger" : "btn-floating waves-effect waves-light btn-add-user btn modal-trigger"}>
-                                        <i className="material-icons"><CSVLink data={wagons} target="_blank" separator={";"}><img alt="xls" src="https://img.icons8.com/color/24/000000/ms-excel.png"/></CSVLink></i>            
-                                   </button>
+                                <ExcelFile element={
+                                        <button className={darkMode ? "btn-floating waves-effect waves-light btn-add-user-table-dark btn modal-trigger" : "btn-floating waves-effect waves-light btn-add-user btn modal-trigger"}>
+                                            <i className="material-icons">
+                                                <img alt="xls" src="https://img.icons8.com/color/24/000000/ms-excel.png"/>    
+                                            </i>            
+                                    </button>
+                                }>       
+                                    <ExcelSheet data={wagons} name="Petroleum">
+                                        <ExcelColumn label="Номер вагона" value="carnumber"/>
+                                        <ExcelColumn label="Состояние парка" value="park_state"/>
+                                        <ExcelColumn label="Станция отправления" value="codestfrom"/>
+                                        <ExcelColumn label="Станция назначения" value="codestdest"/>
+                                        <ExcelColumn label="Дата отправления" value="codestfrom"/>
+                                        <ExcelColumn label="Дата операции" value="departure-date"/>
+                                        <ExcelColumn label="Операция" value="oper_date_last"/>
+                                        <ExcelColumn label="Груз" value="codecargo"/>
+                                        <ExcelColumn label="Вес" value="weight"/>
+                                        <ExcelColumn label="Собственник" value="owner_name"/>
+                                        <ExcelColumn label="Оператор" value="operator_name"/>
+                                        <ExcelColumn label="Грузоотправитель" value="gruz_sender_name"/>
+                                        <ExcelColumn label="Грузоотполучатель" value="gruz_receiver_name"/>
+                                    </ExcelSheet>
+                                </ExcelFile>
                             </div>
                         </div>
                     </div>
